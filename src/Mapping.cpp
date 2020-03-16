bd90aa95e603b28dc9c03c318c1123f2300ddb3f
nvuletic <nenad.vuletic@wolkabout.com>
2020-03-16 14:14:35 +0100

    Additional Mappings

--- src/Mapping.cpp
+++ src/Mapping.cpp
@@ -6,14 +6,12 @@
 
 namespace wolkabout
 {
-Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType, uint16_t address,
-                 bool readRestricted)
+Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType, int16_t address, bool readRestricted)
 : m_reference(reference)
 , m_registerType(registerType)
 , m_address(address)
 , m_readRestricted(readRestricted)
 , m_operationType(OperationType::NONE)
-, m_bitIndex(0)
 {
     switch (m_registerType)
     {
@@ -28,39 +26,37 @@ Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerTyp
     }
 }
 
-Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType, uint16_t address, OutputType type,
+Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType, int16_t address, OutputType type,
                  bool readRestricted)
 : m_reference(reference)
 , m_registerType(registerType)
 , m_address(address)
 , m_readRestricted(readRestricted)
+, m_outputType(type)
 , m_operationType(OperationType::NONE)
-, m_bitIndex(0)
 {
     switch (m_registerType)
     {
     case RegisterType::INPUT_REGISTER:
     case RegisterType::HOLDING_REGISTER_ACTUATOR:
-        if (type != OutputType::INT16 && type != OutputType::UINT16)
+        if (m_outputType != OutputType::INT16 && m_outputType != OutputType::UINT16)
         {
             throw std::logic_error("Single address register mapping can\'t"
                                    " be anything else than INT16, UINT16.");
         }
-        m_outputType = type;
         break;
     case RegisterType::COIL:
     case RegisterType::INPUT_CONTACT:
-        if (type != OutputType::BOOL)
+        if (m_outputType != OutputType::BOOL)
         {
             throw std::logic_error("Single address discrete register can\'t"
                                    " be anything else than BOOL.");
         }
-        m_outputType = type;
         break;
     }
 }
 
-Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType, uint16_t address, uint8_t bitIndex,
+Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType, int16_t address, int8_t bitIndex,
                  bool readRestricted)
 : m_reference(reference)
 , m_registerType(registerType)
@@ -77,18 +73,103 @@ Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerTyp
 }
 
 Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType,
-                 const std::vector<uint16_t>& addresses, OutputType type, OperationType operation, bool readRestricted)
-: m_reference(reference), m_registerType(registerType), m_addresses(addresses), m_readRestricted(readRestricted)
+                 const std::vector<int16_t>& addresses, OutputType type, OperationType operation, bool readRestricted)
+: m_reference(reference)
+, m_registerType(registerType)
+, m_addresses(addresses)
+, m_readRestricted(readRestricted)
+, m_outputType(type)
+, m_operationType(operation)
 {
     if (m_registerType == RegisterType::COIL || m_registerType == RegisterType::INPUT_CONTACT)
     {
         throw std::logic_error("Multi register mapping can\'t be COIL or INPUT_CONTACT.");
     }
 
-    if (type == OutputType::BOOL || type == OutputType::INT16 || type == OutputType::UINT16)
+    if (m_outputType == OutputType::BOOL || m_outputType == OutputType::INT16 || m_outputType == OutputType::UINT16)
     {
-        throw std::logic_error("Multi register mapping can\'t be BOOl, INT16 or UINT16.");
+        throw std::logic_error("Multi register mapping can\'t be BOOL, INT16 or UINT16.");
+    }
+
+    // Can be two registers that are being merged into a 32bit, or multiple ones merging into a string.
+    if (m_operationType == OperationType::MERGE_BIG_ENDIAN || m_operationType == OperationType::MERGE_LITTLE_ENDIAN ||
+        m_operationType == OperationType::MERGE_FLOAT)
+    {
+        if (m_addresses.size() != 2)
+        {
+            throw std::logic_error("Merge operations work only with 2 registers.");
+        }
+
+        if (m_outputType != OutputType::INT32 && m_outputType != OutputType::UINT32 &&
+            m_outputType != OutputType::FLOAT)
+        {
+            throw std::logic_error("Merge operations output 32bit types (INT32, UINT32, FLOAT).");
+        }
+    }
+    else if (m_operationType == OperationType::STRINGIFY)
+    {
+        if (m_outputType != OutputType::STRING)
+        {
+            throw std::logic_error("Stringify can only return string.");
+        }
     }
 }
 
+const std::string& Mapping::getReference() const
+{
+    return m_reference;
+}
+
+bool Mapping::isReadRestricted() const
+{
+    return m_readRestricted;
+}
+
+Mapping::RegisterType Mapping::getRegisterType() const
+{
+    return m_registerType;
+}
+
+int16_t Mapping::getAddress() const
+{
+    return m_address;
+}
+
+const std::vector<int16_t>& Mapping::getAddresses() const
+{
+    return m_addresses;
+}
+
+int16_t Mapping::getStartingAddress() const
+{
+    if (m_address == -1)
+    {
+        return m_addresses.at(0);
+    }
+    return m_address;
+}
+
+int16_t Mapping::getRegisterCount() const
+{
+    if (m_address == -1)
+    {
+        return m_addresses.size();
+    }
+    return 1;
+}
+
+Mapping::OutputType Mapping::getOutputType() const
+{
+    return m_outputType;
+}
+
+Mapping::OperationType Mapping::getOperationType() const
+{
+    return m_operationType;
+}
+
+int8_t Mapping::getBitIndex() const
+{
+    return m_bitIndex;
+}
 }    // namespace wolkabout
--- src/Mapping.h
+++ src/Mapping.h
@@ -5,12 +5,12 @@
 #ifndef WOLKABOUT_MODBUS_MAPPING_H
 #define WOLKABOUT_MODBUS_MAPPING_H
 
-#include "utility/json.hpp"
+#include <cstdint>
+#include <string>
+#include <vector>
 
 namespace wolkabout
 {
-using nlohmann::json;
-
 class Mapping
 {
 public:
@@ -38,27 +38,45 @@ public:
         NONE = 0,
         MERGE_BIG_ENDIAN,
         MERGE_LITTLE_ENDIAN,
+        MERGE_FLOAT,
         STRINGIFY,
         TAKE_BIT
     };
 
     // Default case
-    Mapping(const std::string& reference, RegisterType registerType, uint16_t address, bool readRestricted = false);
+    Mapping(const std::string& reference, RegisterType registerType, int16_t address, bool readRestricted = false);
 
     // Default, with override (allows HR/IR to be both UINT16 and INT16)
-    Mapping(const std::string& reference, RegisterType registerType, uint16_t address, OutputType type,
+    Mapping(const std::string& reference, RegisterType registerType, int16_t address, OutputType type,
             bool readRestricted = false);
 
     // For mappings where you need to fetch a single bit off of a HR/IR.
-    Mapping(const std::string& reference, RegisterType registerType, uint16_t address, uint8_t bitIndex,
+    Mapping(const std::string& reference, RegisterType registerType, int16_t address, int8_t bitIndex,
             bool readRestricted = false);
 
-    // For mapping where there's more registers (not discrete types), .
-    Mapping(const std::string& reference, RegisterType registerType, const std::vector<uint16_t>& addresses,
+    // For mapping where there's more registers (not discrete types).
+    Mapping(const std::string& reference, RegisterType registerType, const std::vector<int16_t>& addresses,
             OutputType type, OperationType operation, bool readRestricted = false);
 
-    explicit Mapping(json::object_t json);
-    explicit Mapping(const std::string& filePath);
+    const std::string& getReference() const;
+
+    bool isReadRestricted() const;
+
+    RegisterType getRegisterType() const;
+
+    int16_t getAddress() const;
+
+    const std::vector<int16_t>& getAddresses() const;
+
+    int16_t getStartingAddress() const;
+
+    int16_t getRegisterCount() const;
+
+    OutputType getOutputType() const;
+
+    OperationType getOperationType() const;
+
+    int8_t getBitIndex() const;
 
 private:
     // General reference data
@@ -67,13 +85,13 @@ private:
 
     // Modbus registers data
     RegisterType m_registerType;
-    uint16_t m_address = -1;
-    std::vector<uint16_t> m_addresses;
+    int16_t m_address = -1;
+    std::vector<int16_t> m_addresses;
 
     // Wolkabout output data
     OutputType m_outputType;
     OperationType m_operationType;
-    uint8_t m_bitIndex = -1;
+    int8_t m_bitIndex = -1;
 };
 }    // namespace wolkabout
 
