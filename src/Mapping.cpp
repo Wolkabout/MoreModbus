d826dff05da6aa3c8f926a91ac60997726efa7e4
nvuletic <nenad.vuletic@wolkabout.com>
2020-03-16 12:29:21 +0100

    Working on Mapping

--- src/Mapping.cpp
+++ src/Mapping.cpp
@@ -3,3 +3,94 @@
 //
 
 #include "Mapping.h"
+
+namespace wolkabout
+{
+Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType,
+        uint16_t address, bool readRestricted)
+: m_reference(reference)
+, m_registerType(registerType)
+, m_address(address)
+, m_readRestricted(readRestricted)
+, m_operationType(OperationType::NONE)
+, m_bitIndex(0)
+{
+    switch (m_registerType) {
+        case RegisterType::INPUT_REGISTER:
+        case RegisterType::HOLDING_REGISTER_ACTUATOR:
+            m_outputType = OutputType::INT16;
+            break;
+        case RegisterType::COIL:
+        case RegisterType::INPUT_CONTACT:
+            m_outputType = OutputType::BOOL;
+            break;
+    }
+}
+
+Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType,
+        uint16_t address, OutputType type, bool readRestricted)
+: m_reference(reference)
+, m_registerType(registerType)
+, m_address(address)
+, m_readRestricted(readRestricted)
+, m_operationType(OperationType::NONE)
+, m_bitIndex(0)
+{
+    switch (m_registerType) {
+        case RegisterType::INPUT_REGISTER:
+        case RegisterType::HOLDING_REGISTER_ACTUATOR:
+            if (type != OutputType::INT16 && type != OutputType::UINT16)
+            {
+                throw std::logic_error("Single address register mapping can\'t"
+                                       " be anything else than INT16, UINT16.");
+            }
+            m_outputType = type;
+            break;
+        case RegisterType::COIL:
+        case RegisterType::INPUT_CONTACT:
+            if (type != OutputType::BOOL)
+            {
+                throw std::logic_error("Single address discrete register can\'t"
+                                       " be anything else than BOOL.");
+            }
+            m_outputType = type;
+            break;
+    }
+}
+
+Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType,
+        uint16_t address, uint8_t bitIndex, bool readRestricted)
+: m_reference(reference)
+, m_registerType(registerType)
+, m_address(address)
+, m_readRestricted(readRestricted)
+, m_bitIndex(bitIndex)
+, m_outputType(OutputType::BOOL)
+, m_operationType(OperationType::TAKE_BIT)
+{
+    if (m_registerType == RegisterType::COIL || m_registerType == RegisterType::INPUT_CONTACT)
+    {
+        throw std::logic_error("Take bit can\'t be done over COIL/INPUT_CONTACT.");
+    }
+}
+
+Mapping::Mapping(const std::string& reference, Mapping::RegisterType registerType,
+        const std::vector<uint16_t>& addresses, OutputType type,
+        OperationType operation, bool readRestricted)
+: m_reference(reference)
+, m_registerType(registerType)
+, m_addresses(addresses)
+, m_readRestricted(readRestricted)
+{
+    if (m_registerType == RegisterType::COIL || m_registerType == RegisterType::INPUT_CONTACT)
+    {
+        throw std::logic_error("Multi register mapping can\'t be COIL or INPUT_CONTACT.");
+    }
+
+    if (type == OutputType::BOOL || type == OutputType::INT16 || type == OutputType::UINT16)
+    {
+        throw std::logic_error("Multi register mapping can\'t be BOOl, INT16 or UINT16.");
+    }
+}
+
+}
--- src/Mapping.h
+++ src/Mapping.h
@@ -5,8 +5,74 @@
 #ifndef WOLKABOUT_MODBUS_MAPPING_H
 #define WOLKABOUT_MODBUS_MAPPING_H
 
-class Mapping
-{
+#include "utility/json.hpp"
+
+namespace wolkabout {
+using nlohmann::json;
+
+class Mapping {
+public:
+    enum class RegisterType
+    {
+        COIL = 0,
+        INPUT_CONTACT,
+        INPUT_REGISTER,
+        HOLDING_REGISTER_ACTUATOR
+    };
+
+    enum class OutputType
+    {
+        BOOL = 0,
+        STRING,
+        INT16,
+        UINT16,
+        INT32,
+        UINT32,
+        FLOAT
+    };
+
+    enum class OperationType
+    {
+        NONE = 0,
+        MERGE_BIG_ENDIAN,
+        MERGE_LITTLE_ENDIAN,
+        STRINGIFY,
+        TAKE_BIT
+    };
+
+    // Default case
+    Mapping(const std::string& reference, RegisterType registerType, uint16_t address,
+            bool readRestricted = false);
+
+    // Default, with override (allows HR/IR to be both UINT16 and INT16)
+    Mapping(const std::string& reference, RegisterType registerType, uint16_t address, OutputType type,
+            bool readRestricted = false);
+
+    // For mappings where you need to fetch a single bit off of a HR/IR.
+    Mapping(const std::string& reference, RegisterType registerType, uint16_t address, uint8_t bitIndex,
+            bool readRestricted = false);
+
+    // For mapping where there's more registers (not discrete types), .
+    Mapping(const std::string& reference, RegisterType registerType, const std::vector<uint16_t>& addresses,
+            OutputType type, OperationType operation, bool readRestricted = false);
+
+    explicit Mapping(json::object_t json);
+    explicit Mapping(const std::string& filePath);
+private:
+    // General reference data
+    std::string m_reference;
+    bool m_readRestricted;
+
+    // Modbus registers data
+    RegisterType m_registerType;
+    uint16_t m_address = -1;
+    std::vector<uint16_t> m_addresses;
+
+    // Wolkabout output data
+    OutputType m_outputType;
+    OperationType m_operationType;
+    uint8_t m_bitIndex = -1;
 };
+}
 
 #endif    // WOLKABOUT_MODBUS_MAPPING_H
