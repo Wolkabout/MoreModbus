45bdd5640a38d5d17ff888b438102fb1ec3af491
nvuletic <nenad.vuletic@wolkabout.com>
2020-04-09 14:53:58 +0200

    Cleaned up IP

--- example/Application.cpp
+++ example/Application.cpp
@@ -91,7 +91,7 @@ int main()
 
     // TCP IP client
     const auto& modbusClient =
-      std::make_shared<wolkabout::LibModbusTcpIpClient>("192.168.0.20", 502, std::chrono::milliseconds(500));
+      std::make_shared<wolkabout::LibModbusTcpIpClient>("<IP_ADDRESS>", 502, std::chrono::milliseconds(500));
 
     const auto& reader = std::make_shared<wolkabout::ModbusReader>(
       *modbusClient, std::chrono::milliseconds(1000));
