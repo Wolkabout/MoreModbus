1138684478dae2adf444775544bf27e0d1991e15
nvuletic <nenad.vuletic@wolkabout.com>
2020-04-17 14:15:21 +0200

    First Test

--- tests/DataParsersTest.cpp
+++ tests/DataParsersTest.cpp
@@ -14,6 +14,12 @@
  * limitations under the License.
  */
 
+#define private public
+#define protected public
+#include "utilities/DataParsers.h"
+#undef private
+#undef protected
+
 #include <gtest/gtest.h>
 
 #include "utilities/ConsoleLogger.h"
@@ -21,20 +27,43 @@
 
 namespace
 {
-class DataParsersTest: public ::testing::Test
+class DataParsersTest : public ::testing::Test
 {
+protected:
+    std::map<uint16_t, std::vector<bool>> bitValues;
+
 public:
     void SetUp()
     {
         auto logger = std::unique_ptr<wolkabout::ConsoleLogger>(new wolkabout::ConsoleLogger());
-        logger->setLogLevel(wolkabout::LogLevel::TRACE);
+        logger->setLogLevel(wolkabout::LogLevel::WARN);
         wolkabout::Logger::setInstance(std::move(logger));
         LOG(DEBUG) << "Started tests.";
+
+        bitValues.emplace(747, std::vector<bool> {0,0,0,0,0,0,1,0,1,1,1,0,1,0,1,1});
+        bitValues.emplace(50616, std::vector<bool> {1,1,0,0,0,1,0,1,1,0,1,1,1,0,0,0});
+        bitValues.emplace(14267, std::vector<bool> {0,0,1,1,0,1,1,1,1,0,1,1,1,0,1,1});
+        bitValues.emplace(2958, std::vector<bool> {0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0});
+        bitValues.emplace(15274, std::vector<bool> {0,0,1,1,1,0,1,1,1,0,1,0,1,0,1,0});
+        bitValues.emplace(52175, std::vector<bool> {1,1,0,0,1,0,1,1,1,1,0,0,1,1,1,1});
+        bitValues.emplace(44510, std::vector<bool> {1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,0});
+        bitValues.emplace(56402, std::vector<bool> {1,1,0,1,1,1,0,0,0,1,0,1,0,0,1,0});
+        bitValues.emplace(20621, std::vector<bool> {0,1,0,1,0,0,0,0,1,0,0,0,1,1,0,1});
+        bitValues.emplace(47701, std::vector<bool> {1,0,1,1,1,0,1,0,0,1,0,1,0,1,0,1});
     }
 };
 
-TEST_F(DataParsersTest, SimpleTest)
+TEST_F(DataParsersTest, TestSeparateBits)
 {
-    LOG(DEBUG) << "Hello World!";
+    for (const auto& kvp: bitValues)
+    {
+        const auto bits = wolkabout::DataParsers::separateBits(kvp.first);
+
+        LOG(DEBUG) << kvp.first;
+        for (uint i = 0; i < 16; i++)
+        {
+            EXPECT_EQ(kvp.second[i], bits[15 - i]);
+        }
+    }
 }
 }    // namespace
