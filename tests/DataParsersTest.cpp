95fdf854b7f27243a464abe8d9dd97d5cca664b9
nvuletic <nenad.vuletic@wolkabout.com>
2020-04-22 09:29:27 +0200

    Fixed more licences

--- tests/DataParsersTest.cpp
+++ tests/DataParsersTest.cpp
@@ -1,17 +1,19 @@
 /*
- * Copyright 2020 WolkAbout Technology s.r.o.
+ * Copyright (C) 2020 WolkAbout Technology s.r.o.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #define private public
@@ -31,8 +33,31 @@ class DataParsersTest : public ::testing::Test
 {
 protected:
     std::map<uint16_t, std::vector<bool>> bitValues;
+    std::map<std::string, std::vector<std::uint16_t>> stringValues;
+    std::map<uint32_t, std::vector<std::uint16_t>> uint32Values;
+    std::map<int32_t, std::vector<std::uint16_t>> int32Values;
+    std::map<float, std::vector<std::uint16_t>> floatValues;
 
 public:
+    void SetUpBitValues()
+    {
+        bitValues.emplace(747, std::vector<bool> {0,0,0,0,0,0,1,0,1,1,1,0,1,0,1,1});
+        bitValues.emplace(2958, std::vector<bool> {0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0});
+        bitValues.emplace(14267, std::vector<bool> {0,0,1,1,0,1,1,1,1,0,1,1,1,0,1,1});
+        bitValues.emplace(15274, std::vector<bool> {0,0,1,1,1,0,1,1,1,0,1,0,1,0,1,0});
+        bitValues.emplace(20621, std::vector<bool> {0,1,0,1,0,0,0,0,1,0,0,0,1,1,0,1});
+        bitValues.emplace(44510, std::vector<bool> {1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,0});
+        bitValues.emplace(47701, std::vector<bool> {1,0,1,1,1,0,1,0,0,1,0,1,0,1,0,1});
+        bitValues.emplace(50616, std::vector<bool> {1,1,0,0,0,1,0,1,1,0,1,1,1,0,0,0});
+        bitValues.emplace(52175, std::vector<bool> {1,1,0,0,1,0,1,1,1,1,0,0,1,1,1,1});
+        bitValues.emplace(56402, std::vector<bool> {1,1,0,1,1,1,0,0,0,1,0,1,0,0,1,0});
+    }
+
+    void SetUpStringValues()
+    {
+
+    }
+
     void SetUp()
     {
         auto logger = std::unique_ptr<wolkabout::ConsoleLogger>(new wolkabout::ConsoleLogger());
@@ -40,16 +65,7 @@ public:
         wolkabout::Logger::setInstance(std::move(logger));
         LOG(DEBUG) << "Started tests.";
 
-        bitValues.emplace(747, std::vector<bool> {0,0,0,0,0,0,1,0,1,1,1,0,1,0,1,1});
-        bitValues.emplace(50616, std::vector<bool> {1,1,0,0,0,1,0,1,1,0,1,1,1,0,0,0});
-        bitValues.emplace(14267, std::vector<bool> {0,0,1,1,0,1,1,1,1,0,1,1,1,0,1,1});
-        bitValues.emplace(2958, std::vector<bool> {0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0});
-        bitValues.emplace(15274, std::vector<bool> {0,0,1,1,1,0,1,1,1,0,1,0,1,0,1,0});
-        bitValues.emplace(52175, std::vector<bool> {1,1,0,0,1,0,1,1,1,1,0,0,1,1,1,1});
-        bitValues.emplace(44510, std::vector<bool> {1,0,1,0,1,1,0,1,1,1,0,1,1,1,1,0});
-        bitValues.emplace(56402, std::vector<bool> {1,1,0,1,1,1,0,0,0,1,0,1,0,0,1,0});
-        bitValues.emplace(20621, std::vector<bool> {0,1,0,1,0,0,0,0,1,0,0,0,1,1,0,1});
-        bitValues.emplace(47701, std::vector<bool> {1,0,1,1,1,0,1,0,0,1,0,1,0,1,0,1});
+        SetUpBitValues();
     }
 };
 
