c9f7acc4800798a3ff0811e053b76b7d49cfb02d
nvuletic <nenad.vuletic@wolkabout.com>
2020-04-28 13:49:58 +0200

    Started RTU tests

---
+++ tests/ModbusSerialRTUClientTest.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 WolkAbout Technology s.r.o.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#define private public
+#define protected public
+#include "modbus/LibModbusTcpIpClient.h"
+#undef private
+#undef protected
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <memory>
+
+#include "mocks/LibModbusMocking.h"
+#include "modbus/libmodbus/modbus.h"
+
+class ModbusSerialRTUClienTest : public ::testing::Test
+{
+
+};
